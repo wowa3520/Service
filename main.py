15b3ee4b4fdb51b907a87adf0db1dfa161221a2b
wowa3520 <wowa3520@gmail.com>
2023-10-19 09:37:29 +0600

    Build apk with new buildozer

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from kivy.app import App
-from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDRectangleFlatButton
 
 
-class MainApp(App):
+class MainApp(MDApp):
     def build(self):
-        return Label(text="Hello, World")
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
 
+        return (
+            MDScreen(
+                MDRectangleFlatButton(
+                    text="Hello, World",
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                )
+            )
+        )
 
 
 MainApp().run()
